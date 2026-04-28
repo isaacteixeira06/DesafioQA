b276cee0efad2b813ab8a53073aa6b86cba548bd
isaacteixeira06 <isaac.manoel06@gmail.com>
2026-04-28 14:20:40 -0300

    chore: configura relatorio html do mochawesome e atualiza gitignore

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    baseUrl: "http://localhost:3000",
-    video: false,
-    screenshotOnRunFailure: true,
-    specPattern: "cypress/**/*.cy.{js,jsx,ts,tsx}", 
+    baseUrl: 'http://localhost:3000', 
     setupNodeEvents(on, config) {
-    }
-  }
+      require('cypress-mochawesome-reporter/plugin')(on);
+      
+      return config;
+    },
+  },
 });
